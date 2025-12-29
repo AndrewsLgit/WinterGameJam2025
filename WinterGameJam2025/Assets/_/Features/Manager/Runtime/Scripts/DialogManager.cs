122f2848ad4d9d1f88684f604eb139438a2d3975
AndrewsLgit <andrewsleruth@gmail.com>
2025-12-29 11:34:29 +0100

    properly getting trigger pressure from stepEvents

--- WinterGameJam2025/Assets/_/Features/Manager/Runtime/Scripts/DialogManager.cs
+++ WinterGameJam2025/Assets/_/Features/Manager/Runtime/Scripts/DialogManager.cs
@@ -36,24 +36,34 @@ namespace Manager.Runtime
             StepDialogEvents = _dialogStepEventContainer.StepDialogEvents;
             _currentStepData.CurrentStepDuration = StepDialogEvents[0].TriggerPressure;
             _eventArrayLength = StepDialogEvents.Length;
+
+            _currentStepData.OnCurrentStepComplete += CurrentStepData_OnStepIndexChanged;
         }
-   
+
+        private void OnDestroy()
+        {
+            _currentStepData.OnCurrentStepComplete -= CurrentStepData_OnStepIndexChanged;
+        }
         #endregion
 
         #region Main Methods
 
-        private void CurrentStepData_OnStepIndexChanged(int oldIndex, int newIndex)
+        private void CurrentStepData_OnStepIndexChanged()
         {
+            _eventIndex = -1;
+            
+            var currentStepIndex = _currentStepData.CurrentStepIndex;
+
             for (int i = 0; i < _eventArrayLength; i++)
             {
-                if ((_currentStepData.CurrentStepIndex < StepDialogEvents[i].DialogEntryRange.From ||
-                     _currentStepData.CurrentStepIndex > StepDialogEvents[i].DialogEntryRange.To) &&
-                    (_currentStepData.CurrentStepIndex < StepDialogEvents[i].DialogExitRange.From ||
-                     _currentStepData.CurrentStepIndex > StepDialogEvents[i].DialogExitRange.To)) continue;
+                var evt = StepDialogEvents[i];
+                bool inEntry = currentStepIndex >= evt.DialogEntryRange.From && _currentStepData.CurrentStepIndex <= evt.DialogEntryRange.To;
+                bool inExit = currentStepIndex >= evt.DialogExitRange.From && _currentStepData.CurrentStepIndex <= evt.DialogExitRange.To;
+                
+                if (!inEntry && !inExit) continue;
                 
                 _eventIndex = i;
-                _currentStepData.CurrentStepDuration = StepDialogEvents[i].TriggerPressure;
-                break;
+                _currentStepData.CurrentStepDuration = evt.TriggerPressure;
             }
         }
    
@@ -61,6 +71,18 @@ namespace Manager.Runtime
 
         #region Utils
 
+        private float ComputeRangeProgress(int stepIndex, StepRange range, float timerProgress)
+        {
+            if (stepIndex < range.From) return 0f;
+            if (stepIndex > range.To) return 1f;
+
+            int rangeLength = range.To - range.From + 1;
+            int stepOffset = stepIndex - range.From;
+            
+            float progress = (stepOffset + timerProgress) / rangeLength;
+            
+            return Mathf.Clamp01(progress);
+        }
    
         #endregion
 
