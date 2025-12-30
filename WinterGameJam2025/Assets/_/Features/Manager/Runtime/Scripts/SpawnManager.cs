31d64ce4905dfc661e04a0211345f27426cb6701
AndrewsLgit <andrewsleruth@gmail.com>
2025-12-30 13:13:14 +0100

    spawn manager spawns/destroys prefabs depending on index

--- WinterGameJam2025/Assets/_/Features/Manager/Runtime/Scripts/SpawnManager.cs
+++ WinterGameJam2025/Assets/_/Features/Manager/Runtime/Scripts/SpawnManager.cs
@@ -1,4 +1,5 @@
 
+using System.Collections.Generic;
 using UnityEngine;
 using Foundation.Runtime;
 using SharedData.Runtime;
@@ -31,11 +32,44 @@ namespace Manager.Runtime
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
+
+        private void Start()
+        {
+            _currentStepData.OnCurrentStepComplete += StepData_OnCurrentStepComplete;
+
+            if (_camera != null) return;
+            _camera = Camera.main;
+        }
         
         #endregion
 
         #region Main Methods
 
+        private void StepData_OnCurrentStepComplete()
+        {
+            foreach (ObjectSpawnEvent spawnEvent in SpawnEvent)
+            {
+                if (spawnEvent.Prefab == null) continue;
+                
+                if (spawnEvent.StepSpawnIndex == _currentStepData.CurrentStepIndex)
+                {
+                    var obj = Instantiate(spawnEvent.Prefab);
+                    obj.transform.position = _camera.transform.position;
+                    var spawnPos = obj.transform.position;
+                    spawnPos.y -= 1.8f;
+                    obj.transform.position = spawnPos;
+                    obj.name = spawnEvent.Prefab.name;
+                    _spawnedObjects.Add(obj);
+                }
+                if (spawnEvent.StepDisappearIndex == _currentStepData.CurrentStepIndex)
+                {
+                    var obj = _spawnedObjects.Find(x => x.name == spawnEvent.Prefab.name);
+                    _spawnedObjects.Remove(obj);
+                    Destroy(obj);
+                }
+                    
+            }
+        }
    
         #endregion
 
@@ -46,7 +80,10 @@ namespace Manager.Runtime
 
         #region Private & Protected
 
-        [SerializeField] private CurrentStep_Data _currentStep;
+        [SerializeField] private CurrentStep_Data _currentStepData;
+        [SerializeField] private Camera _camera;
+        
+        private List<GameObject> _spawnedObjects = new List<GameObject>();
 
         #endregion
     }
